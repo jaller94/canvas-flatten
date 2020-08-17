f5f54a5cb02faf37edd69db4555ca22d48cb987e
Niels Leenheer <niels@leenheer.nl>
2020-08-17 21:24:39 +0200

    Update dependencies

--- src/canvas-flatten.js
+++ src/canvas-flatten.js
@@ -2,7 +2,7 @@
  * Use the ImageData from a Canvas and flatten the image on a solid background
  */
 class CanvasFlatten {
-    /**
+  /**
      * Change the image to grayscale
      *
      * @param  {object}   image         The imageData of a Canvas 2d context
@@ -10,19 +10,19 @@ class CanvasFlatten {
      * @return {object}                 The resulting imageData
      *
      */
-    flatten(image, background) {
-        for (let i = 0; i < image.data.length; i += 4) {
-            let alpha = image.data[i + 3];
-            let invAlpha = 255 - alpha;
+  flatten(image, background) {
+    for (let i = 0; i < image.data.length; i += 4) {
+      const alpha = image.data[i + 3];
+      const invAlpha = 255 - alpha;
 
-            image.data[i] = (alpha * image.data[i] + invAlpha * background[0]) / 255;
-            image.data[i + 1] = (alpha * image.data[i + 1] + invAlpha * background[1]) / 255;
-            image.data[i + 2] = (alpha * image.data[i + 2] + invAlpha * background[2]) / 255;
-            image.data[i + 3] = 0xff;
-        }
-
-        return image;
+      image.data[i] = (alpha * image.data[i] + invAlpha * background[0]) / 255;
+      image.data[i + 1] = (alpha * image.data[i + 1] + invAlpha * background[1]) / 255;
+      image.data[i + 2] = (alpha * image.data[i + 2] + invAlpha * background[2]) / 255;
+      image.data[i + 3] = 0xff;
     }
+
+    return image;
+  }
 }
 
 module.exports = new CanvasFlatten();
